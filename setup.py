2c1aee12e4cb1f40f37ff831ee52ea0812f57682
Krzysiek <krzysiekfonal@gmail.com>
2016-09-15 19:20:41 +0200

    Add license and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='textpipeliner',
+      version='0.1',
+      description='textpipeliner - library for extracting specific words from sentences of a document',
+      long_description='This library allows you to extract specific words(in form of tuple) from every sentence'
+                       'of a document by creating custom structure of extraction processing.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Science/Research',
+      ],
+      keywords='nlp text mining text extraction',
+      url='https://github.com/krzysiekfonal/textpipeliner',
+      author='Krzysztof Fonal',
+      author_email='krzysiek.fonal@gmail.com',
+      license='MIT',
+      packages=['textpipeliner'],
+      install_requires=[
+          'spacy', 'grammaregex'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=True)
